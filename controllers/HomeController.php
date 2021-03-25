6fa28f4bb23911110ef8c4832fb1ca4398786425
xuan_duc <pham.xuan.duc.23.09.2019@gmail.com>
2021-03-25 13:50:03 +0700

    update comment code

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,10 +5,12 @@ class HomeController
 
     public function index()
     {
+        // Goi category product model de lay du lieu 
         require_once('models/CategoryProductModel.php');
         $categoriesProductModel = new CategoryProductModel();
         $categoryProduct = $categoriesProductModel->getCategories();
 
+        // Goi view de tra ve du lieu da lay 
         require_once('views/HomeView.php');
         $categoryProductView = new HomeView();
         $categoryProductView->index($categoryProduct);
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'controllers/HomeController.php';
-
-
+// file nay chay dau tien no chay vo file HomeController
 $homeController = new HomeController;
 $homeController->index();
