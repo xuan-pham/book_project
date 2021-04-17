518e0446ed31206ba782e77ed6403b5edacdaa85
quy <nguyenquy290500@gmail.com>
2021-04-17 14:48:02 +0700

    xonggd

--- index.php
+++ index.php
@@ -95,6 +95,9 @@ else if ($action == 'login') {
     $adminController->editSetting();
 } else if ($action == 'admin-delete-setting') {
     $adminController->deleteSetting();
-}else{
+} else if ($action == 'admin-edit-postCategory') {
+    $adminController->editPostCategory();
+}
+else{
     echo "404 ERROR";
 }
